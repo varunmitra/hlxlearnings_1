15563fa7010203feadb9c078e373e0d72ee9d036
vmitra <vmitra@adobe.com>
2022-10-11 14:54:23 -0400

    changes to accomodate social

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,12 +2,18 @@ import { createOptimizedPicture } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
+  let social = 0;
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.children.length === 1 && div.querySelector('picture') && social == 0) 
+    {
+      div.className = 'cards-card-image';
+      social = 1;
+    } else if(social == 1)
+        div.className ='cards-cards-social';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
