e3475de5f3a8af7d38031cc97ce9e0ab2884a1c8
vmitra <vmitra@adobe.com>
2022-10-11 15:08:48 -0400

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,15 +2,22 @@ import { createOptimizedPicture } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  
+  let social = 0;
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-        
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        if(social == 0){
+          div.className = 'cards-card-image';
+          social = 1;
+    } else if(social > 0){
+      div.className = 'cards-card-social';
+    }
+  }
       else div.className = 'cards-card-body';
+      social = 0;
     });
     ul.append(li);
   });
