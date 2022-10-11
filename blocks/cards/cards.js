8b37a6e5bad754dea6172dbee15a44aa42b3ecff
vmitra <vmitra@adobe.com>
2022-10-11 15:21:51 -0400

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -8,15 +8,13 @@ export default function decorate(block) {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        if(social == 0){
+      if (div.children.length === 1 && div.querySelector('picture') && social == 0) {     
           div.className = 'cards-card-image';
-          social = 1;
-    } else if(social == 1){
-      div.className = 'cards-card-social';
-    }
-  }
-      else div.className = 'cards-card-body';
+          social = 1;   
+     }
+     else if (div.children.length > 2 && div.querySelector('picture') && social == 1) div.className = 'cards-card-social';
+
+     else div.className = 'cards-card-body';
       
     });
     social = 0;
